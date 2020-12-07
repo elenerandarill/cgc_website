73e52571dab64e3628386b29dc8a30fdd1787383
elener <paulina.wojno@gmail.com>
2020-12-07 15:32:57 +0100

    Added working app - landing page, without content.

--- cgc_website/landing_page/views.py
+++ cgc_website/landing_page/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    # return HttpResponse("Hello you")
+
+    # HttpResponse("Hello you")
+    return render(request, "landing_page/home.html")
