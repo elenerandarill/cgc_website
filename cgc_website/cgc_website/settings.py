e76116262f4a8851e03e5170801a2295f3a0decb
elener <paulina.wojno@gmail.com>
2020-12-14 19:00:33 +0100

    changes in setting.py for deployment on AWS.

--- cgc_website/cgc_website/settings.py
+++ cgc_website/cgc_website/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&*7d&2juh#%f$%huwv%$=)v+p_o*0+1m7r&s1j07yxuu=q7e-x'
+SECRET_KEY = os.environ['CARAPI_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
